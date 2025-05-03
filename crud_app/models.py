39835baf7352116f9c59bb0674b45085ee16c57d
Jung-YouHwan <yhwjd99@gmail.com>
2025-05-03 13:43:47 +0900

    feat: models 정의

--- crud_app/models.py
+++ crud_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    member_id: models.BigAutoField = models.BigAutoField(primary_key=True)
+    name: models.CharField = models.CharField(max_length=100)
+    email: models.EmailField = models.EmailField()
+    age: models.IntegerField = models.IntegerField()
+
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'members'
+
+
+class Order(models.Model):
+    order_id: models.BigAutoField = models.BigAutoField(primary_key=True)
+    member_id: models.BigIntegerField = models.BigIntegerField()
+    price: models.IntegerField = models.IntegerField()
+
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'orders'
