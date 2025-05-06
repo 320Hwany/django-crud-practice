4233ca0fb8b8181f0a9ce09e3bcd6b3da37dd5c5
Jung-YouHwan <yhwjd99@gmail.com>
2025-05-06 13:14:37 +0900

    refactor: 토큰 처리 데코레이터 방식으로 수정

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from crud_app.views import MemberView, MemberListView, MemberLoginView
+from crud_app.views import MemberView, MemberLoginView
 
 urlpatterns = [
     path("members", MemberView.as_view(), name="member-view"),
-    path("members/<int:member_id>", MemberListView.as_view(), name="member-detail-view"),
     path("login", MemberLoginView.as_view(), name="login"),
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,8 +1,10 @@
 import logging
 from dataclasses import asdict
+from functools import wraps
 from typing import Any
 
 import jwt
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,16 +15,44 @@ from crud_app.dtos.dtos import MemberCreateRequest, MemberResponse, MemberUpdate
 logger = logging.getLogger(__name__)
 
 def handle_exception(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except ValueError as e:
             return Response({"message": str(e)}, status=400)
+        except AuthenticationFailed as e:
+            return Response({"message": str(e)}, status=401)
         except Exception as e:
             logger.error(f"An error occurred: {e}")
             return Response({"message": "An error occurred"}, status=500)
     return wrapper
 
+def authentication(func):
+    @wraps(func)
+    def wrapper(self, request: Request, *args, **kwargs):
+        secret_key: str = "GZ8x8-Dl2FZAClDee7aOZ5rHnPDO1BtYK8wH6P5wf_k"
+        algorithm: str = "HS256"
+
+        access_token: str = request.headers.get("AccessToken")
+        refresh_token: str = request.headers.get("RefreshToken")
+
+        try:
+            access_payload = jwt.decode(access_token, secret_key, algorithms=[algorithm])
+            member_id = access_payload.get("member_id")
+        except jwt.ExpiredSignatureError:
+            try:
+                refresh_payload = jwt.decode(refresh_token, secret_key, algorithms=[algorithm])
+                member_id = refresh_payload.get("member_id")
+            except jwt.ExpiredSignatureError:
+                raise AuthenticationFailed("Refresh token has expired")
+            except jwt.InvalidTokenError:
+                raise AuthenticationFailed("Invalid refresh token")
+        except jwt.InvalidTokenError:
+            raise AuthenticationFailed("Invalid access token")
+
+        return func(self, request, member_id=member_id, *args, **kwargs)
+    return wrapper
 
 class MemberView(APIView):
 
@@ -37,66 +67,23 @@ class MemberView(APIView):
         return Response("OK", status=201)
 
     @handle_exception
-    def patch(self, request: Request) -> Response:
-        secret_key: str = "GZ8x8-Dl2FZAClDee7aOZ5rHnPDO1BtYK8wH6P5wf_k"
-        algorithm: str = "HS256"
+    @authentication
+    def get(self, request: Request, member_id: int) -> Response:
+        member_response: MemberResponse = self.member_service.get_member(member_id)
+        return Response(asdict(member_response), status=200)
 
-        access_token: str = request.headers.get("AccessToken")
-        refresh_token: str = request.headers.get("RefreshToken")
-
-        try:
-            access_payload = jwt.decode(access_token, secret_key, algorithms=[algorithm])
-            member_id = access_payload.get("member_id")
-        except jwt.ExpiredSignatureError:
-            try:
-                refresh_payload = jwt.decode(refresh_token, secret_key, algorithms=[algorithm])
-                member_id = refresh_payload.get("member_id")
-            except jwt.ExpiredSignatureError:
-                raise ValueError("Refresh token has expired")
-            except jwt.InvalidTokenError:
-                raise ValueError("Invalid refresh token")
-        except jwt.InvalidTokenError:
-            raise ValueError("Invalid access token")
-
+    @handle_exception
+    @authentication
+    def patch(self, request: Request, member_id: int) -> Response:
         member_update_request: MemberUpdateRequest = MemberUpdateRequest(**request.data)
         self.member_service.update_member(member_id, member_update_request)
         return Response("OK", status=200)
 
     @handle_exception
-    def delete(self, request: Request) -> Response:
-        secret_key: str = "GZ8x8-Dl2FZAClDee7aOZ5rHnPDO1BtYK8wH6P5wf_k"
-        algorithm: str = "HS256"
-
-        access_token: str = request.headers.get("AccessToken")
-        refresh_token: str = request.headers.get("RefreshToken")
-
-        try:
-            access_payload = jwt.decode(access_token, secret_key, algorithms=[algorithm])
-            member_id = access_payload.get("member_id")
-        except jwt.ExpiredSignatureError:
-            try:
-                refresh_payload = jwt.decode(refresh_token, secret_key, algorithms=[algorithm])
-                member_id = refresh_payload.get("member_id")
-            except jwt.ExpiredSignatureError:
-                raise ValueError("Refresh token has expired")
-            except jwt.InvalidTokenError:
-                raise ValueError("Invalid refresh token")
-        except jwt.InvalidTokenError:
-            raise ValueError("Invalid access token")
-
+    @authentication
+    def delete(self, request: Request, member_id: int) -> Response:
         self.member_service.delete_member(member_id)
         return Response("OK", status=200)
-
-class MemberListView(APIView):
-
-    def __init__(self, **kwargs: Any):
-        super().__init__(**kwargs)
-        self.member_service = container.member_service
-
-    @handle_exception
-    def get(self, request: Request, member_id: int) -> Response:
-        member_response: MemberResponse = self.member_service.get_member(member_id)
-        return Response(asdict(member_response), status=200)
 
 class MemberLoginView(APIView):
 
