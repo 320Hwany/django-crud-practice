a49e45591996d0ff6721743c58c3a258feea206c
Jung-YouHwan <yhwjd99@gmail.com>
2025-05-06 14:40:15 +0900

    test: 회원 로그인 실패

--- crud_app/tests/member_service_test.py
+++ crud_app/tests/member_service_test.py
@@ -46,6 +46,25 @@ def test_login():
     assert jwt_token.refresh_token is not None
 
 @pytest.mark.django_db
+def test_login_fail():
+    # given
+    member_repository = MemberRepository()
+    member_service = MemberService(member_repository=member_repository)
+    dto = MemberCreateRequest(
+        name="test name",
+        email="test@gmail.com",
+        password="test password",
+        age=20,
+    )
+    member: Member = member_repository.create_member(dto)
+
+    member_login_request: MemberLoginRequest = MemberLoginRequest(email="fail@gmail.com", password="fail password", )
+
+    # when & then
+    with pytest.raises(ValueError, match="회원 정보가 존재하지 않습니다."): member_service.login(member_login_request)
+
+
+@pytest.mark.django_db
 def test_create_token():
     # given
     member_repository = MemberRepository()
