9fa7faedfc27f96292d99a662817f39bf7957d01
Jung-YouHwan <yhwjd99@gmail.com>
2025-05-06 18:04:23 +0900

    refactor: DI 컨테이너 싱글톤 관리

--- crud_app/dependency_container.py
+++ crud_app/dependency_container.py
@@ -1,8 +1,18 @@
+from typing import Any
+
 from crud_app.repository.member_repository import MemberRepository
 from crud_app.service.member_service import MemberService
 
 
-class DependencyContainer:
+class MetaSingleton(type):
+    _instances: dict = {}
+
+    def __call__(cls, *args: Any, **kwargs: Any) -> object:
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class DependencyContainer(metaclass=MetaSingleton):
     def __init__(self):
         self._member_repository = MemberRepository()
         self._member_service = MemberService(self._member_repository)
